2ed640dd9243965c07a843c7f1460c92bff8844a
Tyler Heucke <tyler.heucke@dell.com>
2018-11-14 13:33:17 -0800

    Add initial sources and scripts
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

---
+++ build.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#set -x
+
+work="$(dirname "$0")"
+
+version="$(cat "$work/VERSION")"
+debian_with_opx="${version##*-}"
+
+: "${UPSTREAM:=${version%%-*}}"
+: "${DEBIAN:=${debian_with_opx%opx*}}"
+: "${OPX:=${debian_with_opx#*opx}}"
+: "${PARALLELISM:=$(grep -c ^processor /proc/cpuinfo)}"
+
+# Enable source packages
+sudo sed -i 's/deb \(.*\)/&\ndeb-src \1/' /etc/apt/sources.list
+
+sudo apt update
+sudo apt-get build-dep -y "linux=${UPSTREAM}-${DEBIAN}"
+apt-get source "linux=${UPSTREAM}-${DEBIAN}"
+
+echo cp "${work}/${UPSTREAM}-${DEBIAN}.config" "linux-${UPSTREAM}/.config"
+cp "${work}/${UPSTREAM}-${DEBIAN}.config" "linux-${UPSTREAM}/.config"
+
+env -u ARCH make -C "linux-${UPSTREAM}" "-j$PARALLELISM" bindeb-pkg \
+  LOCALVERSION=-opx \
+  KDEB_PKGVERSION="${UPSTREAM}-${DEBIAN}opx${OPX}"
